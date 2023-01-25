7e28d1d3491e8f0ef893900de3f6753d27735a8a
Ananya Gupta <ananya.it19@nsut.ac.in>
2023-01-25 12:45:49 +0530

    Contact-list 1

--- Contact_list/index.js
+++ Contact_list/index.js
@@ -8,11 +8,33 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+var contactList = [
+    {
+        name: "Ananya",
+        phone: "2950496423"
+    }, 
+    {
+        name: "Yajas",
+        phone: "9570647963"
+    }
+]
+
+
+app.post('/create-contact', function(req,res){
+    return res.redirect('/practice');
+})
+app.get('/practice', function(req,res){
+    // res.send("Server running");
+    return res.render('practice', {title: 'My Contact List'});
+})
 
 app.get('/', function(req,res){
-    // res.send("Server running");
-    return res.render('index', {title: 'My Contact List'});
+    return res.render('index', {
+        title : 'Contact List', 
+        contact_list: contactList
+    })
 })
+
 
 app.listen(port, function(err){
     if(err){
